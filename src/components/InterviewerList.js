42edc5418b0f4a0d6e7493be10bc2d887f326e3c
Paul Chen <paulchen647@gmail.com>
2020-10-27 13:31:14 -0700

    Finish InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,33 @@
 import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
- 
-  return <div></div>;
+  const list = props.interviewers.map((interviewer) => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+    />
+  ));
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {list}
+      </ul>
+    </section>
+  );
 }
+
+// const item = props.days.map((day) => (
+//   <DayListItem
+//     name={day.name}
+//     spots={day.spots}
+//     selected={day.name === props.day}
+//     setDay={props.setDay}
+//   />
+// ));
+// return <ul>{item}</ul>;
