0bfd68ec6eb1d69fffedcc9138b23c0f23c7e6d0
Paul Chen <paulchen647@gmail.com>
2020-11-04 16:42:37 -0800

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 
 import axios from "axios";
 
-if (process.env.REACT_APP_API_BASE_URL) {
-  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-}
+
 
 import "index.scss";
 
 import Application from "components/Application";
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(<Application />, document.getElementById("root"));
